c70024f9d26c5c6e81184219dd1bc5b977d39a4e
t-kazuto <68689391+t-kazuto@users.noreply.github.com>
2020-09-03 18:07:13 +0900

    バリデーション設定

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,16 @@
 class Restaurant < ApplicationRecord
+  has_one_attached :image
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
-  has_one_attached :image
+  
+  with_options presence: true do
+    validates :day
+    validates :prefecture_id, numericality: { other_than: 1 }
+    validates :station
+    validates :name
+    validates :genre
+    validates :menu
+    validates :price
+  end
 end
