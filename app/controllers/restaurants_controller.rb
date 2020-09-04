4a87e4d33ae8932d38d51bfc44309446c4ba9684
t-kazuto <68689391+t-kazuto@users.noreply.github.com>
2020-09-04 19:09:17 +0900

    showアクションをコントローラーに定義

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -16,6 +16,10 @@ class RestaurantsController < ApplicationController
     end
   end
 
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
   private
   def restaurant_params
     params.require(:restaurant).permit(:day, :time, :prefecture_id, :station, :name, :genre, :menu, :price, :image, :remark)
