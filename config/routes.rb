fe16a374e2084a88fa4b30f674a5991dc5cab500
t-kazuto <68689391+t-kazuto@users.noreply.github.com>
2020-09-06 23:22:13 +0900

    ルーティング設定（updateアクション）

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   get 'restaurants/index'
   root to: "restaurants#index"
 
-  resources :restaurants, only: [:index, :new, :create, :show, :edit]
+  resources :restaurants, only: [:index, :new, :create, :show, :edit, :update]
 end
