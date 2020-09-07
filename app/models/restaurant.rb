54a5ded39ac5e81a944841b495ba2958b14089a8
t-kazuto <68689391+t-kazuto@users.noreply.github.com>
2020-09-07 19:07:50 +0900

    バリデーション修正

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -7,7 +7,7 @@ class Restaurant < ApplicationRecord
   with_options presence: true do
     validates :username
     validates :day
-    validates :prefecture_id, numericality: { other_than: 1 }
+    validates :prefecture_id, numericality: { other_than: 1, message: 'Select' }
     validates :station
     validates :name
     validates :genre
