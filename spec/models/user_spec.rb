e723d5bbb28f5cf6e4a90d4a217f8ef992d8bba4
t-kazuto <68689391+t-kazuto@users.noreply.github.com>
2020-09-09 15:15:27 +0900

    ユーザー管理機能単体テストコード

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'ユーザー新規登録' do
+   before do
+    @user = FactoryBot.build(:user)
+   end
+
+   it "username、email、password、password_confirmationが存在すれば登録できる" do
+    expect(@user).to be_valid
+   end
+
+   it 'nicknameが空では登録できない' do
+    @user.username = nil
+    @user.valid?
+    expect(@user.errors.full_messages).to include("Username can't be blank")
+   end
+
+   it 'emailが空では登録できない' do
+    @user.email = nil
+    @user.valid?
+    expect(@user.errors.full_messages).to include("Email can't be blank")
+   end
+
+   it 'passwordが空では登録できない' do
+    @user.password = nil
+    @user.valid?
+    expect(@user.errors.full_messages).to include("Password can't be blank")
+   end
+
+   it 'passwordが存在してもpassword_confirmationが空では登録できない' do
+    @user.password_confirmation = ''
+    @user.valid?
+    expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+   end
+  end
 end
