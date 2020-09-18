5d5a67efc9fd325fbebfd4d117c14c90957beb78
t-kazuto <68689391+t-kazuto@users.noreply.github.com>
2020-09-18 10:03:08 +0900

    commentsコントローラー作成・createアクション定義

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,11 @@
+class CommentsController < ApplicationController
+  def create
+    comment = Comment.create(comment_params)
+    redirect_to "/restaurants/#{comment.restaurant.id}" 
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, restaurant_id: params[:restaurant_id])
+  end
+end
