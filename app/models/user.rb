19dc9d040e4723286c4cdfb5e9344620b460ead8
t-kazuto <68689391+t-kazuto@users.noreply.github.com>
2020-09-18 09:44:12 +0900

    アソシエーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :restaurants
+  has_many :comments
 
   with_options presence: true do
     validates :username
